731c260fed5eea6ad29815a42bb1072747c901de
예님 <blueming574@naver.com>
2025-05-02 12:12:36 +0900

    Update send.py

--- send.py
+++ send.py
@@ -5,15 +5,16 @@ WEBHOOK_URL = "https://discordapp.com/api/webhooks/1367394347380768798/hzwiJLVkL
 
 # 한국 시간 구하기
 kst = datetime.utcnow() + timedelta(hours=9)
-now = kst.strftime("%m/%d %H:%M")
+now = kst.strftime("%m/%d %H:%M")     # 표시용 (00/00 00:00)
+time_only = kst.strftime("%H:%M")     # 비교용 ("18:00" 등)
 
 # 현재 시각별로 메시지 결정
 alert = ""
 
-if now in ["02:55", "05:55", "08:55", "11:55", "14:55", "17:55", "20:55", "23:55"]:
+if time_only in ["02:55", "05:55", "08:55", "11:55", "14:55", "17:55", "20:55", "23:55"]:
     alert = f"@everyone\n🔔 **[츄르봇 알림]**\n\n`{now}` │ 😨 불길한 소환의 결계 5분 전! 모두 사냥터로 모이세요!"
 
-elif now in ["12:00", "18:00", "20:00", "22:00"]:
+elif time_only in ["12:00", "18:00", "20:00", "22:00"]:
     alert = f"@everyone\n🔔 **[츄르봇 알림]**\n\n`{now}` │ ⚔️ 필드 보스 등장 시간입니다! **<늑대의 숲-페리>, <여신의 뜰-크라브바흐>, <얼음 협곡-크라마>"
 
 # 메시지가 있으면 전송
